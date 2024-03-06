1bbc85071acae2c554582de1d628912f3ae87d71
Klaudia Skóra <skora.klaudia.dev@gmail.com>
2024-03-06 13:17:02 +0100

    add logging about response time

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.requestTime = new Date().getTime();
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
   // Do something with response data
+  console.log(
+    `Response time: ${new Date().getTime() - response.config.requestTime} ms`
+  );
   return response;
 });
 
